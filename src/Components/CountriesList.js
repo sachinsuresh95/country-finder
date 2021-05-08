b0f0919fac783e7d952c218dfa895aa0a02b3e7a
Sachin Suresh <sachinelp@gmail.com>
2021-05-08 11:56:28 +0530

    No results message

--- src/Components/CountriesList.js
+++ src/Components/CountriesList.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import LazyLoad, { forceCheck } from "react-lazyload";
 import { debounce } from "react-debounce-render";
 import compose from "@ramda/compose";
+import "twin.macro";
 import withCountries from "../utils/hoc/withCountries";
 import CountryCard from "./CountryCard";
 import CardContainer from "./CardContainer";
@@ -9,6 +10,9 @@ import Loading from "./Loading";
 
 const CountriesList = ({ countries, filterKey = "" }) => {
   const { list, loading } = countries;
+  const listToDisplay = list.filter((country) =>
+    country.name.toLowerCase().includes(filterKey.toLowerCase())
+  );
 
   useEffect(() => {
     console.log({ filterKey });
@@ -21,15 +25,15 @@ const CountriesList = ({ countries, filterKey = "" }) => {
         <Loading />
       ) : (
         <CardContainer>
-          {list
-            .filter((country) =>
-              country.name.toLowerCase().includes(filterKey.toLowerCase())
-            )
-            .map((country) => (
+      {listToDisplay.length ? (
+            listToDisplay.map((country) => (
               <LazyLoad height={400} key={country.name}>
                 <CountryCard country={country} />
               </LazyLoad>
-            ))}
+            ))
+          ) : (
+            <div tw="px-4">No Results</div>
+          )}
         </CardContainer>
       )}
     </>
